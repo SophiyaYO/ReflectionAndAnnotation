1da2f4df220d3266807ceebd165b8e68b051de8a
SophYo <sophiya.dimitrova@gmail.com>
2019-07-24 17:59:15 +0300

    reflections checking

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Class reflection = Reflection.class;
+
+        System.out.println(reflection);
+
+        Class superClass = reflection.getSuperclass();
+
+        System.out.println(superClass);
+
+        Class[] interfaces = reflection.getInterfaces();
+
+        for (Class anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+
     }
 }
